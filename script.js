7c1c8684daa1f5488b6edcf285ebd98a8e5da1c0
MasterChief1-117 <oscar.alejandrom2006@gmail.com>
2024-10-19 12:39:13 -0400

    mostrar resultado en la pagina

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+let bton = document.getElementById("btn");
+let numArreglo = [20]
+let mensaje = document.createElement("p");
+let pe = document.getElementById('texto');
+bton.addEventListener('click', (e) => {
+    for(let cont = 0; cont <= 20; cont++){
+        numArreglo[cont] = cont;
+        if (numArreglo[cont]%2 == 0){
+            let messenger = document.createTextNode(`${numArreglo[cont]} es par `);
+            mensaje.appendChild(messenger);
+            document.body.insertBefore(mensaje, pe);
+        } else{
+            let messenger = document.createTextNode(`${numArreglo[cont]} es par `);
+            mensaje.appendChild(messenger);
+            document.body.insertBefore(mensaje, pe);
+        }
+    }
+});
+
 console.log("hola mundo")
 
-for (let cont=0; cont<=20; cont++){
-    if (cont%2 === 0){
-        console.log(cont+ " es par")
-    }else{
-        console.log(cont + " es impar")
-    }
-    
-}
+
