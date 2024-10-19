0c10a16ae1f0e35ebd9298bc6b1bf299fa8fb9f8
MasterChief1-117 <oscar.alejandrom2006@gmail.com>
2024-10-19 11:59:07 -0400

    operacion de par impar

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hola mundo")
+
+for (let cont=0; cont<=20; cont++){
+    if (cont%2 === 0){
+        console.log(cont+ " es par")
+    }else{
+        console.log(cont + " es impar")
+    }
+    
+}
